f84d32e3222fd5e2668f965258ae129688d3811d
Hervé <61780905+Herve-D@users.noreply.github.com>
2024-05-03 00:30:22 +0200

    fix responsive

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,8 +17,9 @@ import { FeedModule } from './components/feed/feed.module';
 
 const materialModules = [
   MatButtonModule,
-  MatToolbarModule,
   MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
 ]
 
 @NgModule({
